e05cc243503e6e84c56c640333937ef8e8e5b34b
ThinkPowerful <sudheendran.lakshman@gmail.com>
2018-08-29 21:09:30 +0530

    first commit

---
+++ Demo/qmetry/src/test/java/qmetry/ValidateLoginTest.java
@@ -0,0 +1,13 @@
+package qmetry;
+
+import org.testng.annotations.Test;
+
+public class ValidateLoginTest {
+	
+	@Test
+	public void validateLogin()
+	{
+		System.out.println("Login Validated");
+	}
+
+}
---
+++ Demo/qmetry/src/test/java/qmetry/ValidateTitleTest.java
@@ -0,0 +1,13 @@
+package qmetry;
+
+import org.testng.annotations.Test;
+
+public class ValidateTitleTest {
+
+	@Test
+	public void validateTitle()
+	{
+		System.out.println("Title Validated");
+	}
+	
+}
