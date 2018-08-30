3442075f07240822b7e53aba394b4465ea3f0ceb
C47273 <C47273@NLVHPRAABV00193.solon.prd>
2018-08-30 07:26:27 +0200

    Updated

--- Demo/qmetry/src/test/java/qmetry/ValidateLoginTest.java
+++ Demo/qmetry/src/test/java/qmetry/ValidateLoginTest.java
@@ -1,13 +1,34 @@
 package qmetry;
 
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ValidateLoginTest {
+	WebDriver driver =null;
 	
 	@Test
 	public void validateLogin()
 	{
-		System.out.println("Login is Validated");
+		System.setProperty("webdriver.ie.driver", System.getProperty("user.dir")+"\\ExeFiles\\IEDriverServer.exe");
+		driver = new InternetExplorerDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.get("http://www.google.com");
+		boolean title = driver.getTitle().contains("Google");
+		Assert.assertTrue(title);
+		
+		 try {
+			 Runtime.getRuntime().exec("taskkill /F /IM IEDriverServer.exe");
+			 } catch (IOException e) {
+			 e.printStackTrace();
+			 }
+		
 	}
 
 }
--- Demo/qmetry/src/test/java/qmetry/ValidateTitleTest.java
+++ Demo/qmetry/src/test/java/qmetry/ValidateTitleTest.java
@@ -1,13 +1,35 @@
 package qmetry;
 
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ValidateTitleTest {
-
+WebDriver driver =null;
+	
 	@Test
 	public void validateTitle()
 	{
-		System.out.println("Title is Validated");
+		System.setProperty("webdriver.ie.driver", System.getProperty("user.dir")+"\\ExeFiles\\IEDriverServer.exe");
+		driver = new InternetExplorerDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.get("http://www.google.com");
+		boolean title = driver.getTitle().contains("PHPTRAVELS");
+		Assert.assertTrue(title);
+		
+		 try {
+			 Runtime.getRuntime().exec("taskkill /F /IM IEDriverServer.exe");
+			 } catch (IOException e) {
+			 e.printStackTrace();
+			 }
 	}
 	
 }
